4d67470c7cf31a75f891386cd8774a761ba73df9
mayunfeng <mayunfeng@cookids.com.cn>
2019-04-28 10:55:57 +0800

    bug fix

--- frontend/utils/CommonUtils.js
+++ frontend/utils/CommonUtils.js
@@ -1,6 +1,4 @@
-export default {
 
-    isMobileDevice() {
-        return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-    }
+export function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 }
